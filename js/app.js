dc033fcfbeb6facf94f342bfd8500bfa10e4ab1c
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-03-01 14:44:39 +0600

    details feature added

--- js/app.js
+++ js/app.js
@@ -10,9 +10,17 @@ const allPhones = () => {
 
 const showDisplay = (phones) => {
     // console.log(phones);
-    for(const phone of phones){
+    const morePhones = phones;
+    const phone20 = morePhones.slice(0, 20);
+    // console.log(phone20);
+    const parentElement = document.getElementById('phone-container');
+    parentElement.textContent = '';
+    if(phone20.length == 0){
+       alert('no result found');
+    }
+    else{
+    for(const phone of phone20){
         // console.log(phone);
-        const parentElement = document.getElementById('phone-container');
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.classList.add('mb-5');
@@ -20,7 +28,7 @@ const showDisplay = (phones) => {
         <div class="card" style="width: 18rem;">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
+                <h4 class="card-title">${phone.phone_name}</h4>
                 <p class="card-text">${phone.brand}</p>
                 <button onclick="details('${phone.slug}')" class="btn btn-info text-light">Show More</button>
             </div>
@@ -29,27 +37,40 @@ const showDisplay = (phones) => {
     parentElement.append(div);  
     }
 };
+}
+    
 
 const details = (phoneId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhoneDetail(data.data));
-}
+};
 
 const displayPhoneDetail = data => {
-    console.log(data);
+    // console.log(data);
     const phoneDetails = document.getElementById('phone-details');
-    
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.innerHTML = `
     <div>
         <img src="${data.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${data.name}</h5>
+            <h4 "class="card-title">${data.name}</h4>
+            <h4 class="card-title">Release-Date: ${data.releaseDate} </h4>
+            <h6>Storage: ${data.mainFeatures.chipSet}</h6>
+            <h6>Display-Size: ${data.mainFeatures.displaySize}</h6>
+            <h6>Memory: ${data.mainFeatures.memory}</h6>
+            <h6>Storage: ${data.mainFeatures.storage}</h6>
+            <p>${data.mainFeatures.sensors}</p>
+            <h6>WLAN: ${data.others.WLAN}</h6>
+            <h6>Bluetooth: ${data.others.Bluetooth}</h6>
+            <h6>GPS: ${data.others.GPS}</h6>
         </div>
     </div>    
      `;
     phoneDetails.appendChild(div);
-}
+};
+
+
 
